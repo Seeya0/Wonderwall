3a24e3cd744a6b064108d66f10d079619e5aa4aa
yoshioka.seiya <yoshioka.seiya@hc-sys.jp>
2022-01-18 11:42:53 +0900

    Headerコンポーネント、stylesを追加

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,38 @@
+import { AppBar, Box, InputBase, Toolbar, Typography } from '@material-ui/core';
+import { Autocomplete } from '@react-google-maps/api';
 import React from 'react';
 
-const Header = () => {
+import useStyles from './styles.js';
+import SearchIcon from '@material-ui/icons/Search';
+
+const Header = ({ onPlaceChanged, onLoad }) => {
+  //styles.jsからuseStylesを呼ぶ。cssのコードはstylesに集約。
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Header is ///</h1>
-    </div>
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
