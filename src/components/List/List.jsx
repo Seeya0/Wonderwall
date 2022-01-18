84dcaad13c349bb0c971aeb1a59b54424ad21a52
Seeya0 <vinlandtorfin@gmail.com>
2022-01-18 23:33:03 +0900

    Listコンポーネントを追加

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import React, { useState } from 'react';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
+import useStyles from './styles';
 const List = () => {
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const places = [
+    { name: 'Cool Place' },
+    { name: 'Best Beer' },
+    { name: 'Best Pub!!' },
+  ];
   return (
-    <div>
-      <h1>Listやで</h1>
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels & Attractions around you!
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>above 3.0</MenuItem>
+          <MenuItem value={4}>above 4.0</MenuItem>
+          <MenuItem value={4.5}>above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,27 @@
+import { useMediaQuery } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
 import React from 'react';
 
+import useStyles from './styles';
+
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width: 600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div>
-      <h1>マップだよん</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
     </div>
   );
 };
